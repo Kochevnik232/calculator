64b92c6fef2b088e4f1bab6321ec45f1e842b442
Владимир Смирнов <kochevnik232@gmail.com>
2023-12-04 14:59:09 +0300

    release

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def main(input_str: str) -> str:
+    try:
+        var = list(map(str, input_str.split()))
+        num1 = int(var[0])
+        num2 = int(var[2])
+
+        if len(var) != 3 or var[1] not in {'+', '-', '*', '/'}:
+            return "throws Exception: Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)"
+
+        if not (1 <= num1 <= 10) or not (1 <= num2 <= 10):
+            return "throws Exception: Введенные числа меньше 0 или больше 10"
+
+        result = None
+        if var[1] == "+":
+            result = num1 + num2
+        elif var[1] == "-":
+            result = num1 - num2
+        elif var[1] == "*":
+            result = num1 * num2
+        elif var[1] == "/":
+            result = num1 // num2
+        return result
+
+    except:
+        return str(result)
+
+
+user_input = input("Введите пример в формате 1 + 2: ")
+result = main(user_input)
+print(result)
